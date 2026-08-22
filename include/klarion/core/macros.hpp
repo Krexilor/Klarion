bc9f6b830273b1a971a7e3c792d1179a119738a9
Aarav Malik <krexilor.dev@gmail.com>
2026-08-22 12:55:09 +0530

    feat: add structured logging macros (LOG_INFO_KV and friends)

--- include/klarion/core/macros.hpp
+++ include/klarion/core/macros.hpp
@@ -2,6 +2,7 @@
 
 // LIBRARIES ---------------------------------------------------------------------------------------------------------------------------------------|
 #include <string>
+#include <vector>
 #include <utility>
 
 // EXTERNAL LIBRARIES ------------------------------------------------------------------------------------------------------------------------------|
@@ -9,9 +10,11 @@
 
 // PROJECT HEADER FILES ----------------------------------------------------------------------------------------------------------------------------|
 #include "klarion/klarion.hpp"
+#include "klarion/core/field.hpp"
 
 // MACROS DEFINATION -------------------------------------------------------------------------------------------------------------------------------|
 namespace klarion {
+    // Log a message with fmt-style formatting
     template<typename... Args>
     inline void log(Level level, SourceLocation location, fmt::format_string<Args...> fmt, Args&&... args) {
         auto logger = default_logger();
@@ -21,12 +24,31 @@ namespace klarion {
         }
     }
 
+    // Log a message to a specific logger
     template<typename... Args>
     inline void log_to(const std::string& logger_name, Level level, SourceLocation location, fmt::format_string<Args...> fmt, Args&&... args) {
         auto logger = get(logger_name);
         if (logger && is_enabled(level, logger->level())) {
             std::string message = fmt::format(fmt, std::forward<Args>(args)...);
             logger->log(level, std::move(message), location);
+        }
+    }
+
+    // Log a plain message with structured key-value fields
+    template<typename... Fields>
+    inline void log_kv(Level level, SourceLocation location, std::string message, Fields&&... fields) {
+        auto logger = default_logger();
+        if (logger && is_enabled(level, logger->level())) {
+            logger->log(level, std::move(message), location, std::vector<Field>{std::forward<Fields>(fields)...});
+        }
+    }
+
+    // Log a plain message with structured key-value fields to a specific logger
+    template<typename... Fields>
+    inline void log_to_kv(const std::string& logger_name, Level level, SourceLocation location, std::string message, Fields&&... fields) {
+        auto logger = get(logger_name);
+        if (logger && is_enabled(level, logger->level())) {
+            logger->log(level, std::move(message), location, std::vector<Field>{std::forward<Fields>(fields)...});
         }
     }
 }
@@ -46,3 +68,19 @@ namespace klarion {
 #define LOG_TO_WARN(logger_name, ...) klarion::log_to(logger_name, klarion::Level::Warn, KLARION_SOURCE_LOCATION(), __VA_ARGS__)
 #define LOG_TO_ERROR(logger_name, ...) klarion::log_to(logger_name, klarion::Level::Error, KLARION_SOURCE_LOCATION(), __VA_ARGS__)
 #define LOG_TO_CRITICAL(logger_name, ...) klarion::log_to(logger_name, klarion::Level::Critical, KLARION_SOURCE_LOCATION(), __VA_ARGS__)
+
+// STRUCTURED LOGGING MACROS -----------------------------------------------------------------------------------------------------------------------|
+#define LOG_TRACE_KV(msg, ...) klarion::log_kv(klarion::Level::Trace, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_DEBUG_KV(msg, ...) klarion::log_kv(klarion::Level::Debug, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_INFO_KV(msg, ...) klarion::log_kv(klarion::Level::Info, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_WARN_KV(msg, ...) klarion::log_kv(klarion::Level::Warn, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_ERROR_KV(msg, ...) klarion::log_kv(klarion::Level::Error, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_CRITICAL_KV(msg, ...) klarion::log_kv(klarion::Level::Critical, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+
+// STRUCTURED LOGGER-SPECIFIC MACROS ---------------------------------------------------------------------------------------------------------------|
+#define LOG_TO_TRACE_KV(logger_name, msg, ...) klarion::log_to_kv(logger_name, klarion::Level::Trace, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_TO_DEBUG_KV(logger_name, msg, ...) klarion::log_to_kv(logger_name, klarion::Level::Debug, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_TO_INFO_KV(logger_name, msg, ...) klarion::log_to_kv(logger_name, klarion::Level::Info, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_TO_WARN_KV(logger_name, msg, ...) klarion::log_to_kv(logger_name, klarion::Level::Warn, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_TO_ERROR_KV(logger_name, msg, ...) klarion::log_to_kv(logger_name, klarion::Level::Error, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
+#define LOG_TO_CRITICAL_KV(logger_name, msg, ...) klarion::log_to_kv(logger_name, klarion::Level::Critical, KLARION_SOURCE_LOCATION(), msg, __VA_ARGS__)
