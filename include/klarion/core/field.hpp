50a83723c588dc92e1b5d1079f2ed56b8bd1bdea
Aarav Malik <krexilor.dev@gmail.com>
2026-08-23 20:54:18 +0530

    feat: preserve field value type (string, int, float, bool) for typed sink output

--- include/klarion/core/field.hpp
+++ include/klarion/core/field.hpp
@@ -2,23 +2,42 @@
 
 // LIBRARIES ---------------------------------------------------------------------------------------------------------------------------------------|
 #include <string>
+#include <type_traits>
 
 // EXTERNAL LIBRARIES ------------------------------------------------------------------------------------------------------------------------------|
 #include <fmt/format.h>
 
 // FIELD DEFINITION --------------------------------------------------------------------------------------------------------------------------------|
 namespace klarion {
+    enum class FieldKind {
+        String, Int, Float, Bool
+    };
+
     struct Field {
         std::string key;
         std::string value;
+        FieldKind kind{FieldKind::String};
     };
 
     template<typename T>
     inline Field kv(std::string key, const T& value) {
-        return Field{std::move(key), fmt::format("{}", value)};
+        using DecayedT = std::decay_t<T>;
+
+        if constexpr (std::is_same_v<DecayedT, bool>) {
+            return Field{std::move(key), value ? "true" : "false", FieldKind::Bool};
+        }
+        else if constexpr (std::is_floating_point_v<DecayedT>) {
+            return Field{std::move(key), fmt::format("{}", value), FieldKind::Float};
+        }
+        else if constexpr (std::is_integral_v<DecayedT>) {
+            return Field{std::move(key), fmt::format("{}", value), FieldKind::Int};
+        }
+        else {
+            return Field{std::move(key), fmt::format("{}", value), FieldKind::String};
+        }
     }
 
     inline Field kv(std::string key, std::string value) {
-        return Field{std::move(key), std::move(value)};
+        return Field{std::move(key), std::move(value), FieldKind::String};
     }
 }
